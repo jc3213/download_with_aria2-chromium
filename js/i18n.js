55a05e36755b1acfcbb79ca69678ef7a3369ae53
jc3213 <jc3213@users.noreply.github.com>
2020-06-12 12:44:31 +0800

    a much better approach to context warning

--- js/i18n.js
+++ js/i18n.js
@@ -2,6 +2,6 @@ $('[i18n]').each((index, element) => {
     $(element).html(chrome.i18n.getMessage(element.innerHTML));
 });
 
-$('#__i18n__ > span').each((index, element) => {
-    window[element.id] = $(element).html();
+$('#__i18n__ ').html((index, text) => {
+    text.match(/\w+/g).map(item => window[item] = chrome.i18n.getMessage(item));
 });
