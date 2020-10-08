38ffb9127c683a308f26bc53502f9505ce1ba7be
jc3213 <jc3213@users.noreply.github.com>
2020-10-08 10:29:20 +0800

    update task manager
    Click file name in file list to copy its url (non-bittorrent downloads only)
    Pre-implement function for restart stopped downloads

--- js/taskmgr.js
+++ js/taskmgr.js
@@ -7,8 +7,8 @@ $('div.taskQueue').on('click', (event) => {
         $('#taskDetails').show();
         printTaskDetails(gid);
     }
-    else if (event.target.id === 'copy_btn') {
-        getDownloadURLs(gid);
+    else if (event.target.id === 'retry_btn') {
+        retryTask(gid);
     }
     else if (event.target.id === 'remove_btn') {
         removeTask(status, gid);
@@ -16,60 +16,61 @@ $('div.taskQueue').on('click', (event) => {
     else if (event.target.id === 'progress_btn') {
         toggleTask(status, gid);
     }
+});
 
-    function getDownloadURLs(gid) {
-        var url = $(event.target).attr('uri');
-        navigator.clipboard.writeText(url);
-        showNotification(window['warn_url_copied'], url);
+function removeTask(status, gid) {
+    if (['active', 'waiting', 'paused'].includes(status)) {
+        var method = 'aria2.forceRemove';
     }
+    else if (['complete', 'error', 'removed'].includes(status)) {
+        method = 'aria2.removeDownloadResult';
+    }
+    else {
+        return console.log(status);
+    }
+    jsonRPCRequest({'method': method, 'gid': gid});
+}
 
-    function removeTask(status, gid) {
-        if (['active', 'waiting', 'paused'].includes(status)) {
-            var method = 'aria2.forceRemove';
-        }
-        else if (['complete', 'error', 'removed'].includes(status)) {
-            method = 'aria2.removeDownloadResult';
-        }
-        else {
-            return console.log(status);
-        }
-        jsonRPCRequest({'method': method, 'gid': gid});
+function toggleTask(status, gid) {
+    if (['active', 'waiting'].includes(status)) {
+        var method = 'aria2.pause';
     }
+    else if (status === 'paused') {
+        method = 'aria2.unpause';
+    }
+    else if (['complete', 'error', 'removed'].includes(status)) {
+        method = 'aria2.removeDownloadResult';
+    }
+    else {
+        return console.log(status);
+    }
+    jsonRPCRequest({'method': method, 'gid': gid});
+}
 
-    function toggleTask(status, gid) {
-        if (['active', 'waiting'].includes(status)) {
-            var method = 'aria2.pause';
+function printTaskDetails(gid) {
+    jsonRPCRequest(
+        {'method': 'aria2.tellStatus', 'gid': gid},
+        (result) => {
+            taskManager = setInterval(() => refreshTaskDetails(result.gid), 1000);
+            printTaskName(result);
+            printTaskOption(result.gid);
+            var decimal = result.files.length.toString().length;
+            var taskFiles = result.files.map(item => item = '<tr><td>'
+            +           multiDecimalNumber(item.index, decimal) + '</td><td title="' + item.path.replace(/\//g, '\\') + '"'
+            +           (result.files[0].uris.length > 0 ? ' uri="' + result.files[0].uris[0].uri + '">' : '>')
+            +           item.path.split('/').pop() + '</td><td>'
+            +           bytesToFileSize(item.length) + '</td><td>'
+            +           ((item.completedLength / item.length * 10000 | 0) / 100).toString() + '%</td></tr>'
+            );
+            $('#taskFiles').html('<table>' + taskFiles.join('') + '</table>').find('td:nth-child(2)').on('click', (event) => {
+                var uri = $(event.target).attr('uri');
+                if (uri) {
+                    navigator.clipboard.writeText(uri);
+                    showNotification(window['warn_url_copied'], uri);
+                }
+            });
         }
-        else if (status === 'paused') {
-            method = 'aria2.unpause';
-        }
-        else if (['complete', 'error', 'removed'].includes(status)) {
-            method = 'aria2.removeDownloadResult';
-        }
-        else {
-            return console.log(status);
-        }
-        jsonRPCRequest({'method': method, 'gid': gid});
-    }
-
-    function printTaskDetails(gid) {
-        jsonRPCRequest(
-            {'method': 'aria2.tellStatus', 'gid': gid},
-            (result) => {
-                taskManager = setInterval(() => refreshTaskDetails(result.gid), 1000);
-                printTaskName(result);
-                printTaskOption(result.gid)
-                var decimal = result.files.length.toString().length;
-                var taskFiles = result.files.map(item => item = '<tr><td>'
-                +           multiDecimalNumber(item.index, decimal) + '</td><td title="' + item.path.replace(/\//g, '\\') + '">'
-                +           item.path.split('/').pop() + '</td><td>'
-                +           bytesToFileSize(item.length) + '</td><td>'
-                +           ((item.completedLength / item.length * 10000 | 0) / 100).toString() + '%</td></tr>'
-                );
-                $('#taskFiles').html('<table>' + taskFiles.join('') + '</table>');
-            }
-        );
-    }
+    );
 
     function refreshTaskDetails(gid) {
         jsonRPCRequest(
@@ -77,9 +78,7 @@ $('div.taskQueue').on('click', (event) => {
             (result) => {
                 printTaskName(result);
                 var completeRatio = result.files.map(item => ((item.completedLength / item.length * 10000 | 0) / 100).toString() + '%');
-                $('#taskFiles').find('td:nth-child(4)').each((index, element) => {
-                    $(element).html(completeRatio[index]);
-                });
+                $('#taskFiles').find('td:nth-child(4)').each((index, element) => $(element).html(completeRatio[index]));
             }
         );
     }
@@ -94,7 +93,7 @@ $('div.taskQueue').on('click', (event) => {
         $('#optionUpload').attr({'gid': result.gid, 'disabled': !bittorrent || complete});
         $('#optionProxy').attr({'gid': result.gid, 'disabled': bittorrent || complete});
     }
-});
+}
 
 function printTaskOption(gid) {
     jsonRPCRequest(
@@ -104,7 +103,20 @@ function printTaskOption(gid) {
             $('#optionUpload').val(result['max-upload-limit']);
             $('#optionProxy').val(result['all-proxy'] || '');
         }
-    )
+    );
+}
+
+function retryTask(gid) {
+    jsonRPCRequest([
+            {'method': 'aria2.getFiles', 'gid': gid},
+            {'method': 'aria2.getOption', 'gid': gid}
+        ],
+        (result, options) => {
+            showNotification('Caution', 'This function is not available at this moment');
+            // https://github.com/ziahamza/webui-aria2/blob/dddf776a18b095ac373a22b28135c5dd6858496b/docs/app.js#L1989
+            // Wait for porting
+        }
+    );
 }
 
 $('#taskName').on('click', (event) => {
