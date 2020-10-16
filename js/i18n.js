da6e7683390950d7f82a0363fb79c16a230a4eff
jc3213 <jc3213@users.noreply.github.com>
2020-10-16 09:15:44 +0800

    Dropped jQuery API library

--- js/i18n.js
+++ js/i18n.js
@@ -1,11 +1,7 @@
-$('[i18n]').each((index, element) => {
-    $(element).html(chrome.i18n.getMessage(element.innerHTML));
-});
+document.querySelectorAll('[i18n]').forEach(item => item.innerHTML = chrome.i18n.getMessage(item.innerHTML));
 
-$('[i18n_title]').each((index, element) => {
-    $(element).attr('title', chrome.i18n.getMessage(element.title));
-});
+document.querySelectorAll('[i18n_title]').forEach(item => item.title = chrome.i18n.getMessage(item.title));
 
 [
     'warn_aria2_version', 'warn_url_copied'
-].map(item => window[item] = chrome.i18n.getMessage(item));
+].forEach(item => window[item] = chrome.i18n.getMessage(item));
