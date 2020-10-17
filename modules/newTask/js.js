129e922204c41e1ccdfe52687b54a2b777a47401
jc3213 <jc3213@users.noreply.github.com>
2020-10-17 23:19:18 +0800

    some clean up

--- modules/newTask/js.js
+++ modules/newTask/js.js
@@ -2,9 +2,9 @@ document.getElementById('setProxy').addEventListener('click', (event) => {
     document.getElementById('taskProxy').disabled = !document.getElementById('taskProxy').disabled;
 });
 
-document.querySelector('#submit_btn').addEventListener('click', (event) => {
+document.getElementById('submit_btn').addEventListener('click', (event) => {
     var referer = document.getElementById('taskReferer').value;
-    var proxy = document.getElementById('setProxy').checked ? document.querySelector('#taskProxy').value : '';
+    var proxy = document.getElementById('setProxy').checked ? document.getElementById('taskProxy').value : '';
     var url = document.getElementById('taskBatch').value.split('\n').forEach(item => { if (item !== '') downWithAria2({'url': item, 'referer': referer, 'proxy': proxy}) });
     window.parent.window.postMessage('newTaskWindow');
 });
