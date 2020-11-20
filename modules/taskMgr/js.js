73d97f48a66948ca7b04c64e2e73cd6fc00a742d
jc3213 <jc3213@users.noreply.github.com>
2020-11-20 09:29:23 +0800

    reverted inline script due to CSP problem

--- modules/taskMgr/js.js
+++ modules/taskMgr/js.js
@@ -29,8 +29,7 @@ function printTaskDetails(gid) {
         var filePath = info.path.replace(/\//g, '\\');
         var fileSize = bytesToFileSize(info.length);
         var fileRatio = ((info.completedLength / info.length * 10000 | 0) / 100).toString() + '%';
-        var clickEvent = bittorrent ? '' : 'copyFileUrl(\'' + fileUrl + '\')';
-        return '<tr onclick="' + clickEvent + '"><td>' + info.index + '</td><td title="' + filePath + '">' + filename + '</td><td>' + fileSize + '</td><td>' + fileRatio + '</td></tr>';
+        return '<tr uri="' + fileUrl + '"><td>' + info.index + '</td><td title="' + filePath + '">' + filename + '</td><td>' + fileSize + '</td><td>' + fileRatio + '</td></tr>';
     }
 }
 
@@ -65,10 +64,13 @@ document.getElementById('taskName').addEventListener('click', (event) => {
     clearInterval(taskManager);
 });
 
-function copyFileUrl(url) {
-    navigator.clipboard.writeText(url);
-    showNotification(window['warn_url_copied'], url);
-}
+document.getElementById('taskFiles').addEventListener('click', (event, uri) => {
+    document.querySelectorAll('tr').forEach((item, index)=> { if (item.contains(event.target)) uri = item.getAttribute('uri'); });
+    if (uri) {
+        navigator.clipboard.writeText(uri);
+        showNotification(window['warn_url_copied'], uri);
+    }
+});
 
 var taskManager;
 var gid
