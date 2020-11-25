eed48e3d7eefa0482996173d4751f3a5a4ca348b
jc3213 <jc3213@users.noreply.github.com>
2020-11-25 17:17:09 +0800

    clean up

--- components/common.js
+++ components/common.js
@@ -105,7 +105,7 @@ function downWithAria2(session) {
                 showNotification('Downloading', session.url);
             },
             (error, rpc) => {
-                showNotification(error, rpc || session.url || 'No URI to download');
+                showNotification(error, rpc || session.url);
             }
         );
     }
