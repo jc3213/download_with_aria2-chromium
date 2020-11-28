55565f1bf0786202ffeafb8c4ad83c5686f00cda
jc3213 <jc3213@users.noreply.github.com>
2020-11-28 13:23:11 +0800

    reverted i18n logic
    reverted: https://github.com/jc3213/download_with_aria2-chromium/commit/7fb9c593badcaf6e0645a310dc6e56c5ea8d463c

--- components/i18n.js
+++ components/i18n.js
@@ -1,13 +1,5 @@
-document.querySelectorAll('[i18n]').forEach(item => {
-    var message = item.getAttribute('i18n');
-    item.innerHTML = chrome.i18n.getMessage(message);
-});
+document.querySelectorAll('[i18n]').forEach(item => item.innerHTML = chrome.i18n.getMessage(item.innerHTML));
 
-document.querySelectorAll('[i18n_title]').forEach(item => {
-    var message = item.getAttribute('i18n_title');
-    item.title = chrome.i18n.getMessage(message);
-});
+document.querySelectorAll('[i18n_title]').forEach(item => item.title = chrome.i18n.getMessage(item.title));
 
-[
-    'warn_aria2_version', 'warn_url_copied'
-].forEach(item => window[item] = chrome.i18n.getMessage(item));
+['warn_aria2_version', 'warn_url_copied'].forEach(item => window[item] = chrome.i18n.getMessage(item));
