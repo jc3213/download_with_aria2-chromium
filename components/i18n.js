091a6f118863b2d5e6ddb8fd3d8ab4bc2f4489a0
jc3213 <jc3213@users.noreply.github.com>
2021-01-07 14:13:54 +0800

    reduce innerHTML usage

--- components/i18n.js
+++ components/i18n.js
@@ -1,3 +1,3 @@
-document.querySelectorAll('[i18n]').forEach(item => item.innerHTML = chrome.i18n.getMessage(item.innerHTML));
+document.querySelectorAll('[i18n]').forEach(item => item.innerText = chrome.i18n.getMessage(item.innerText));
 
 document.querySelectorAll('[i18n_title]').forEach(item => item.title = chrome.i18n.getMessage(item.title));
