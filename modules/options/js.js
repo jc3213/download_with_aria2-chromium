661f3c3b8779f07c6bb84ef5330d90d3860ed97b
jc3213 <jc3213@users.noreply.github.com>
2021-04-08 21:41:20 +0800

    changed selector, ui for add new uri to task

--- modules/options/js.js
+++ modules/options/js.js
@@ -11,17 +11,17 @@ menuTabs.forEach(active => {
     });
 });
 
-document.getElementById('export').addEventListener('click', (event) => {
+document.querySelector('#export').addEventListener('click', (event) => {
     var blob = new Blob([JSON.stringify(localStorage)], {type: 'application/json; charset=utf-8'});
-    var saver = document.getElementById('saver');
+    var saver = document.querySelector('#saver');
     saver.href = URL.createObjectURL(blob);
     saver.download = 'downwitharia2_options-' + new Date().toLocaleString('ja').replace(/[\/\s:]/g, '_') + '.json';
     saver.click();
 });
 
-document.getElementById('import').addEventListener('click', (event) => document.getElementById('reader').click());
+document.querySelector('#import').addEventListener('click', (event) => document.querySelector('#reader').click());
 
-document.getElementById('reader').addEventListener('change', (event) => {
+document.querySelector('#reader').addEventListener('change', (event) => {
     var reader = new FileReader();
     reader.readAsText(event.target.files[0]);
     reader.onload = () => {
@@ -48,7 +48,7 @@ document.getElementById('reader').addEventListener('change', (event) => {
     menu.addEventListener('change', (event) => { localStorage[id] = event.target.value; });
 });
 
-document.getElementById('verify').addEventListener('click', (event) => {
+document.querySelector('#verify').addEventListener('click', (event) => {
     jsonRPCRequest(
         {method: 'aria2.getVersion'},
         (result) => {
@@ -60,29 +60,29 @@ document.getElementById('verify').addEventListener('click', (event) => {
     );
 });
 
-document.getElementById('insight').addEventListener('click', (event) => {
+document.querySelector('#insight').addEventListener('click', (event) => {
     if (event.target.classList.contains('checked')) {
-        document.getElementById('token').setAttribute('type', 'password');
+        document.querySelector('#token').setAttribute('type', 'password');
     }
     else {
-        document.getElementById('token').setAttribute('type', 'text');
+        document.querySelector('#token').setAttribute('type', 'text');
     }
     event.target.classList.toggle('checked');
 });
 
-document.getElementById('capture').addEventListener('change', captureFilters);
+document.querySelector('#capture').addEventListener('change', captureFilters);
 captureFilters();
 
-document.getElementById('sizeEntry').addEventListener('change', calcFileSize);
+document.querySelector('#sizeEntry').addEventListener('change', calcFileSize);
 
-document.getElementById('sizeUnit').addEventListener('change', calcFileSize);
+document.querySelector('#sizeUnit').addEventListener('change', calcFileSize);
 
 function captureFilters() {
     if (localStorage['capture'] === '1') {
-        document.getElementById('captureFilters').style.display = 'block';
+        document.querySelector('#captureFilters').style.display = 'block';
     }
     else {
-        document.getElementById('captureFilters').style.display = 'none';
+        document.querySelector('#captureFilters').style.display = 'none';
     }
 }
 
