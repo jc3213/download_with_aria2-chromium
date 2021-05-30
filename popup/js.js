jc3213/download_with_aria2-chromium

commit 098314d27684e8c2abdc3f1bc2a1e8e03a497c6d

make task queue more static

popup/index.html
@@ -9,63 +9,59 @@
     </head>
     <body>
         <div class="frame">
-            <div id="menuTop" class="offside">
+            <div id="upper" class="offside">
                 <span id="newTask_btn" module="newTaskWindow" window="/modules/newTask/index.html" class="button" i18n>popup_new_button</span>
                 <span id="purdge_btn" class="button" i18n>popup_purge_button</span>
             </div>
-            <div id="queueTabs">
-                <span id="active_btn" class="active tab" i18n>popup_active_button</span>
+            <div id="tabs">
+                <span queue="active" class="active tab" i18n>popup_active_button</span>
                 <span id="numActive">0</span>
-                <span id="waiting_btn" class="waiting tab" i18n>popup_waiting_button</span>
+                <span queue="waiting paused" class="waiting tab" i18n>popup_waiting_button</span>
                 <span id="numWaiting">0</span>
-                <span id="stopped_btn" class="removed tab" i18n>popup_stopped_button</span>
+                <span queue="error removed complete" class="removed tab" i18n>popup_stopped_button</span>
                 <span id="numStopped">0</span>
             </div>
-            <div id="networkStatus" class="error" style="display: none;"></div>
+            <div id="network" class="error" style="display: none;"></div>
         </div>
         <hr>
-        <div id="taskQueue">
-            <div id="activeQueue" tab="active_btn"></div>
-            <div id="waitingQueue" tab="waiting_btn"></div>
-            <div id="stoppedQueue" tab="stopped_btn"></div>
-            <div id="template" class="session">
-                <div class="body">
-                    <div>
-                        <span id="name" class="title"></span>
-                        <span id="error"></span>
+        <div id="queue"></div>
+        <div id="template" class="session">
+            <div class="body">
+                <div>
+                    <span id="name" class="title"></span>
+                    <span id="error"></span>
+                </div>
+                <div class="detail">
+                    <div>🖥️<span id="local"></span></div>
+                    <div>⏲️<span id="infinite">∞</span>
+                        <span id="estimate">
+                            <span><label id="day"></label><sub i18n>time_day</sub></span>
+                            <span><label id="hour"></label><sub i18n>time_hour</sub></span>
+                            <span><label id="minute"></label><sub i18n>time_minute</sub></span>
+                            <span><label id="second"></label><sub i18n>time_second</sub></span>
+                        </span>
                     </div>
-                    <div class="detail">
-                        <div>🖥️<span id="local"></span></div>
-                        <div>⏲️<span id="infinite">∞</span>
-                            <span id="estimate">
-                                <span><label id="day"></label><sub i18n>time_day</sub></span>
-                                <span><label id="hour"></label><sub i18n>time_hour</sub></span>
-                                <span><label id="minute"></label><sub i18n>time_minute</sub></span>
-                                <span><label id="second"></label><sub i18n>time_second</sub></span>
-                            </span>
-                        </div>
-                        <div>📦<span id="remote"></span></div>
-                        <div>📶️<span id="connect"></span></div>
-                        <div>⏬<span id="download"></span></div>
-                        <div>⏫<span id="upload"></span></div>
-                    </div>
+                    <div>📦<span id="remote"></span></div>
+                    <div>📶️<span id="connect"></span></div>
+                    <div>⏬<span id="download"></span></div>
+                    <div>⏫<span id="upload"></span></div>
                 </div>
-                <div class="menu">
-                    <span class="button" id="remove_btn">❌</span>
-                    <span class="button" id="invest_btn">🔍</span>
-                    <span class="button" id="retry_btn">🌌</span>
-                </div>
-                <div id="fancybar">
-                    <div id="ratio"></div>
-                </div>
+            </div>
+            <div class="menu">
+                <span class="button" id="remove_btn">❌</span>
+                <span class="button" id="invest_btn">🔍</span>
+                <span class="button" id="retry_btn">🌌</span>
+            </div>
+            <div id="fancybar">
+                <div id="ratio"></div>
             </div>
         </div>
         <hr>
         <div class="frame">
-            <div id="menuBottom" class="offside">
+            <div class="offside">
                 <span id="options_btn" module="optionsWindow" window="/modules/options/index.html" class="button" i18n>popup_options_button</span>
             </div>
-            <div id="networkBandwidth">
+            <div id="bandwidth">
                 <span class="active" i18n>popup_download</span>
                 <span id="downloadSpeed">0 B/s</span>
                 <span class="complete" i18n>popup_upload</span>

popup/js.js
@@ -17,22 +17,21 @@ document.querySelectorAll('span[module]').forEach(module => {
     });
 });
 
-document.querySelectorAll('span.tab').forEach(tab => {
-    tab.addEventListener('click', (event) => {
-        document.querySelectorAll('div[tab]').forEach(body => {
-            var tabId = body.getAttribute('tab');
-            if (tab.classList.contains('checked')) {
-                body.style.display = 'block';
-            }
-            else if (tabId === tab.id) {
-                body.style.display = 'block';
+document.querySelectorAll('[queue]').forEach((active, index, tabs) => {
+    var queue = active.getAttribute('queue');
+    active.addEventListener('click', (event) => {
+        document.querySelectorAll('#queue > div').forEach(task => {
+            task.style.display = active.classList.contains('checked') ? 'block'
+                               : queue.includes(task.status) ? 'block' : 'none';
+        });
+        tabs.forEach(tab => {
+            if (tab == active && !tab.classList.contains('checked')) {
+                tab.classList.add('checked');
             }
             else {
-                body.style.display = 'none';
-                document.getElementById(tabId).classList.remove('checked');
+                tab.classList.remove('checked');
             }
         });
-        tab.classList.toggle('checked');
     });
 });
 
@@ -57,17 +56,17 @@ function printMainFrame() {
         document.querySelector('#numStopped').innerText = global.numStopped;
         document.querySelector('#downloadSpeed').innerText = bytesToFileSize(global.downloadSpeed) + '/s';
         document.querySelector('#uploadSpeed').innerText = bytesToFileSize(global.uploadSpeed) + '/s';
-        document.querySelector('#queueTabs').style.display = 'block';
-        document.querySelector('#menuTop').style.display = 'block';
-        document.querySelector('#networkStatus').style.display = 'none';
+        document.querySelector('#tabs').style.display = 'block';
+        document.querySelector('#upper').style.display = 'block';
+        document.querySelector('#network').style.display = 'none';
         active.forEach(active => printTaskInfo(active, document.querySelector('#activeQueue')));
         waiting.forEach(active => printTaskInfo(active, document.querySelector('#waitingQueue')));
         stopped.forEach(active => printTaskInfo(active, document.querySelector('#stoppedQueue')));
     }, (error, rpc) => {
-        document.querySelector('#queueTabs').style.display = 'none';
-        document.querySelector('#menuTop').style.display = 'none';
-        document.querySelector('#networkStatus').innerText = error;
-        document.querySelector('#networkStatus').style.display = 'block';
+        document.querySelector('#tabs').style.display = 'none';
+        document.querySelector('#upper').style.display = 'none';
+        document.querySelector('#network').innerText = error;
+        document.querySelector('#network').style.display = 'block';
     });
 }
 
@@ -86,7 +85,6 @@ function printTaskInfo(result, queue) {
     task.querySelector('#ratio').innerText = task.querySelector('#ratio').style.width = ((result.completedLength / result.totalLength * 10000 | 0) / 100) + '%';
     task.querySelector('#ratio').className = result.status;
     task.querySelector('#retry_btn').style.display = !result.bittorrent && ['error', 'removed'].includes(result.status) ? 'inline-block' : 'none';
-    queue.appendChild(task);
 }
 
 function appendTaskInfo(result) {
@@ -97,6 +95,7 @@ function appendTaskInfo(result) {
     task.querySelector('#invest_btn').addEventListener('click', (event) => openTaskMgrWindow(result.gid));
     task.querySelector('#retry_btn').addEventListener('click', (event) => removeTaskAndRetry(result.gid));
     task.querySelector('#fancybar').addEventListener('click', (event) => pauseOrUnpauseTask(result.gid, task.status));
+    document.querySelector('#queue').appendChild(task);
     return task;
 }
 

popup/css.css
@@ -1,16 +1,16 @@
-#queueTabs span:nth-child(2n), #networkBandwidth span:nth-child(2n) {
+#tabs span:nth-child(2n), #bandwidth span:nth-child(2n) {
     margin-right: 10px;
 }
 
-#networkStatus {
+#network {
     padding: 3px;
 }
 
-#networkBandwidth span:nth-child(2n+1) {
+#bandwidth span:nth-child(2n+1) {
     padding: 3px 5px;
 }
 
-#taskQueue {
+#queue {
     height: 410px;
     overflow-y: auto;
 }

commit 4cccf6fad74c256a47b6773eb3adaf34408d0562

fixed purge_btn broken in 098314d

popup/index.html
@@ -18,7 +18,7 @@
                 <span id="numActive">0</span>
                 <span queue="waiting paused" class="waiting tab" i18n>popup_waiting_button</span>
                 <span id="numWaiting">0</span>
-                <span queue="error removed complete" class="removed tab" i18n>popup_stopped_button</span>
+                <span queue="complete error removed" class="removed tab" i18n>popup_stopped_button</span>
                 <span id="numStopped">0</span>
             </div>
             <div id="network" class="error" style="display: none;"></div>

popup/js.js
@@ -39,7 +39,11 @@ document.querySelector('#purdge_btn').addEventListener('click', (event) => {
     jsonRPCRequest(
         {method: 'aria2.purgeDownloadResult'},
         (result) => {
-            document.querySelector('#stoppedQueue').innerHTML = '';
+            document.querySelectorAll('#queue > div').forEach(task => {
+                if (['complete', 'error', 'removed'].includes(task.status)) {
+                    task.remove();
+                }
+            });
         }
     );
 });
@@ -131,7 +135,7 @@ function removeTaskFromQueue(gid, status) {
     else {
         return;
     }
-    if (['complete', 'paused', 'error', 'removed'].includes(status)) {
+    if (['complete', 'error', 'removed', 'paused'].includes(status)) {
         var clear = (result) => {
             document.getElementById(gid).remove();
         };