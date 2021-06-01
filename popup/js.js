jc3213/download_with_aria2-chromium

commit 7bfe998568f3c3de2448a7a9be31c03e19a5a3a2

changed variables

popup/js.js
@@ -60,9 +60,9 @@ function printMainFrame() {
         document.querySelector('#tabs').style.display = 'block';
         document.querySelector('#upper').style.display = 'block';
         document.querySelector('#network').style.display = 'none';
-        active.forEach(active => printTaskInfo(active, document.querySelector('#queue > #active')));
-        waiting.forEach(waiting => printTaskInfo(waiting, document.querySelector('#queue > #waiting')));
-        stopped.forEach(stopped => printTaskInfo(stopped, document.querySelector('#queue > #stopped')));
+        active.forEach(active => printTaskInfo(active, document.querySelector('[queue="active"]')));
+        waiting.forEach(waiting => printTaskInfo(waiting, document.querySelector('[queue="waiting"]')));
+        stopped.forEach(stopped => printTaskInfo(stopped, document.querySelector('[queue="stopped"]')));
     }, (error, rpc) => {
         document.querySelector('#tabs').style.display = 'none';
         document.querySelector('#upper').style.display = 'none';

popup/index.html
@@ -25,9 +25,9 @@
         </div>
         <hr>
         <div id="queue">
-            <div id="active" tab="active_btn"></div>
-            <div id="waiting" tab="waiting_btn"></div>
-            <div id="stopped" tab="stopped_btn"></div>
+            <div queue="active" tab="active_btn"></div>
+            <div queue="waiting" tab="waiting_btn"></div>
+            <div queue="stopped" tab="stopped_btn"></div>
         </div>
         <div id="template" class="session">
             <div class="body">

commit 520160d6fb0590811d34ac7fef5f75796020a933

fixed broken purge button in 7bfe998

popup/js.js
@@ -40,7 +40,7 @@ document.querySelector('#purdge_btn').addEventListener('click', (event) => {
     jsonRPCRequest(
         {method: 'aria2.purgeDownloadResult'},
         (result) => {
-            document.querySelector('#queue > #stopped').innerHTML = '';
+            document.querySelector('[queue="stopped"]').innerHTML = '';
         }
     );
 });