b98ae74cc494b3e7f613ca7476e54075f91ddf3d
jc3213 <jc3213@users.noreply.github.com>
2021-06-04 09:21:50 +0800

    optimized tabs and buttons

--- libs/ui/tabs.js
+++ libs/ui/tabs.js
@@ -1,15 +1,18 @@
-document.querySelectorAll('.tab').forEach(tab => {
+document.querySelectorAll('[tab]').forEach(tab => {
+    var active = tab.getAttribute('tab');
     tab.addEventListener('click', (event) => {
-        document.querySelectorAll('[tab]').forEach(body => {
-            var id = body.getAttribute('tab');
-            if (id === tab.id) {
-                tab.classList.add('checked');
-                body.style.display = 'block';
-            }
-            else {
-                body.style.display = 'none';
-                document.getElementById(id).classList.remove('checked');
-            }
-        });
+        if (!tab.classList.contains('checkd')) {
+            document.querySelectorAll('[panel]').forEach(panel => {
+                var id = panel.getAttribute('panel');
+                if (id === active) {
+                    tab.className = 'checked';
+                    panel.style.display = 'block';
+                }
+                else {
+                    panel.style.display = 'none';
+                    document.querySelector('[tab="' + id + '"]').removeAttribute('class');
+                }
+            });
+        }
     });
 });
