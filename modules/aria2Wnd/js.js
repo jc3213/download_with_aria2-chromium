d43638c0ea8845428dd8fb81c8d55777bbc9da95
jc3213 <jc3213@users.noreply.github.com>
2021-06-26 11:58:59 +0800

    #4 part 3 fixed `options`, and `aria2Wnd`

--- modules/aria2Wnd/js.js
+++ modules/aria2Wnd/js.js
@@ -6,7 +6,8 @@ document.querySelector('#back_btn').addEventListener('click', (event) => {
     frameElement.remove();
 });
 
-chrome.runtime.sendMessage({jsonrpc: true}, aria2RPC => {
+chrome.runtime.sendMessage({jsonrpc: true}, response => {
+    aria2RPC = response;
     document.querySelectorAll('[aria2]').forEach(aria2 => parseValueToOption(aria2, aria2RPC.globalOption));
 });
 
