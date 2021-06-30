7a9c20d4aff8c71245a80c5b8c70ab3166ba18f1
jc3213 <jc3213@users.noreply.github.com>
2021-06-30 09:09:04 +0800

    update message system part1 `background`

--- background.js
+++ background.js
@@ -1,7 +1,8 @@
 var aria2RPC = {};
+var aria2Port;
 
 function aria2RPCRequest(request) {
-    var requestJSON = Array.isArray(request) ? request : [request];
+    var requestJSON = Array.isArray(request) ? request.map(createJSON) : [createJSON(request)];
     return fetch(aria2RPC.options.jsonrpc['uri'], {method: 'POST', body: JSON.stringify(requestJSON)}).then(response => {
         if (response.ok) {
             return response.json();
@@ -20,18 +21,23 @@ function aria2RPCRequest(request) {
     });
 }
 
+function createJSON({method, params}) {
+    return {id: '', jsonrpc: 2, method, params : params ? [aria2RPC.options.jsonrpc['token'], ...params] : [aria2RPC.options.jsonrpc['token']]};
+}
+
 function registerMessageService() {
     clearInterval(aria2RPC.connect);
+    clearInterval(aria2RPC.message);
     aria2RPC.connect = setInterval(() => {
         aria2RPCRequest([
-            {id: '', jsonrpc: 2, method: 'aria2.getVersion', params: [aria2RPC.options.jsonrpc['token']]},
-            {id: '', jsonrpc: 2, method: 'aria2.getGlobalOption', params: [aria2RPC.options.jsonrpc['token']]},
-            {id: '', jsonrpc: 2, method: 'aria2.getGlobalStat', params: [aria2RPC.options.jsonrpc['token']]},
-            {id: '', jsonrpc: 2, method: 'aria2.tellActive', params: [aria2RPC.options.jsonrpc['token']]},
-            {id: '', jsonrpc: 2, method: 'aria2.tellWaiting', params: [aria2RPC.options.jsonrpc['token'], 0, 999]},
-            {id: '', jsonrpc: 2, method: 'aria2.tellStopped', params: [aria2RPC.options.jsonrpc['token'], 0, 999]},
-            {id: '', jsonrpc: 2, method: 'aria2.tellStatus', params: [aria2RPC.options.jsonrpc['token'], aria2RPC.lastSession]},
-            {id: '', jsonrpc: 2, method: 'aria2.getOption', params: [aria2RPC.options.jsonrpc['token'], aria2RPC.lastSession]}
+            {method: 'aria2.getVersion'},
+            {method: 'aria2.getGlobalOption'},
+            {method: 'aria2.getGlobalStat'},
+            {method: 'aria2.tellActive'},
+            {method: 'aria2.tellWaiting', params: [0, 999]},
+            {method: 'aria2.tellStopped', params: [0, 999]},
+            {method: 'aria2.tellStatus', params: [aria2RPC.lastSession]},
+            {method: 'aria2.getOption', params: [aria2RPC.lastSession]}
         ]).then(([version, globalOption, globalStat, active, waiting, stopped, sessionResult, sessionOption]) => {
             aria2RPC = {...aria2RPC, version, globalOption, globalStat, active, waiting, stopped, sessionResult, sessionOption, error: undefined};
             chrome.browserAction.setBadgeText({text: globalStat.numActive === '0' ? '' : globalStat.numActive});
@@ -41,6 +47,11 @@ function registerMessageService() {
             clearInterval(aria2RPC.connect);
         });
     }, 1000);
+    aria2RPC.message = setInterval(() => {
+        if (aria2Port) {
+            aria2Port.postMessage(aria2RPC);
+        }
+    }, aria2RPC.options.jsonrpc['refresh']);
 }
 
 chrome.contextMenus.create({
@@ -106,35 +117,32 @@ chrome.runtime.onInstalled.addListener(async (details) => {
 });
 
 chrome.runtime.onConnect.addListener(port => {
-    port.postMessage(aria2RPC);
-    aria2RPC.message = setInterval(() => {
-        port.postMessage(aria2RPC);
-    }, aria2RPC.options.jsonrpc['refresh']);
+    aria2Port = port;
     port.onDisconnect.addListener(() => {
-        clearInterval(aria2RPC.message);
+        aria2Port = null;
     })
 });
 
-chrome.runtime.onMessage.addListener(({jsonrpc, download, request, restart, session, purge}, sender, response) => {
+chrome.runtime.onMessage.addListener(({jsonrpc, session, download, request}, sender, response) => {
     if (jsonrpc) {
         response(aria2RPC);
     }
     if (download) {
         startDownload(...download);
     }
-    if (request) {
-        aria2RPCRequest(request);
-        response();
-    }
-    if (restart) {
-        aria2RPCRequest(restart).then(restartDownload);
-        response();
-    }
-    if (session !== undefined) {
+    if (session) {
         aria2RPC.lastSession = session;
     }
-    if (purge) {
-        aria2RPC.stopped = [];
+    if (request) {
+        aria2RPCRequest(request).then(result => {
+            var {restart, refresh} = request;
+            if (restart) {
+                restartDownload();
+            }
+            if (refresh && aria2Port) {
+                aria2Port.postMessage(aria2RPC);
+            }
+        });
     }
 });
 
@@ -204,7 +212,7 @@ function restartDownload() {
 };
 
 function downloadWithAria2(url, options) {
-    aria2RPCRequest({id: '', jsonrpc: 2, method: 'aria2.addUri', params: [aria2RPC.options.jsonrpc['token'], url, options]})
+    aria2RPCRequest({method: 'aria2.addUri', params: [url, options]})
         .then(response => showNotification(url[0]))
         .catch(showNotification);
 }
